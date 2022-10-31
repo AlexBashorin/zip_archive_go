6ba5768eeda24f8efce85f485bbacf5064b7b564
Earn <alexander.bashorin@yandex.ru>
2022-10-31 22:01:57 +0400

    начал переделывать на FormData (ParseForm), тк base64 дорогой

--- zipit/main.go
+++ zipit/main.go
@@ -4,68 +4,75 @@ import (
 	"archive/zip"
 	"bufio"
 	"encoding/base64"
-	"encoding/json"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func check(e error) {
 	if e != nil {
-		panic(e)
+		log.Fatal(e)
 	}
+}
+
+type Zipit struct {
+	Name, Body string
 }
 
 func main() {
 	// write zip
-	type Zipit struct {
-		Name, Body string
-	}
-
 	http.HandleFunc("/zipit", func(w http.ResponseWriter, r *http.Request) {
-		b, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
+		// PARSE FORM
+		// var buf bytes.Buffer
+		errPF := r.ParseMultipartForm(32 << 20)
+		// errPF := r.ParseForm()
+		check(errPF)
 
-		jsonstr := string(b)
-		var files []Zipit
-		json.Unmarshal([]byte(jsonstr), &files)
+		file, header, errFile := r.FormFile("fileOne")
+		check(errFile)
+		defer file.Close()
 
-		for i, bod := range files {
-			str := bod.Body
-			data, err := base64.StdEncoding.DecodeString(str)
-			if err != nil {
-				log.Fatal("error:", err)
-			}
-			files[i].Body = string(data)
-		}
+		nameFile := strings.Split(header.Filename, ".")
+		ff, errFF := ioutil.ReadFile(header.Filename)
+		check(errFF)
+
+		// b, err := io.ReadAll(r.Body)
+		// check(err)
+
+		// jsonstr := string(b)
+		// var files []Zipit
+		// json.Unmarshal([]byte(jsonstr), &files)
+
+		// for i, bod := range files {
+		// 	str := bod.Body
+		// 	data, err := base64.StdEncoding.DecodeString(str)
+		// 	if err != nil {
+		// 		log.Fatal("error:", err)
+		// 	}
+		// 	files[i].Body = string(data)
+		// }
 
 		archive, errArch := os.Create("archive.zip")
-		if errArch != nil {
-			panic(errArch)
-		}
+		check(errArch)
 		defer archive.Close()
 
 		wArch := zip.NewWriter(archive)
 
-		for _, file := range files {
-			f, err := wArch.Create(file.Name)
-			if err != nil {
-				log.Fatal(err)
-			}
-			_, err = f.Write([]byte(file.Body))
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
+		f, err := wArch.Create(nameFile[0])
+		check(err)
+		_, err = f.Write(ff)
+		check(err)
+		// for _, file := range files {
+		// 	f, err := wArch.Create(file.Name)
+		// 	check(err)
+		// 	_, err = f.Write([]byte(file.Body))
+		// 	check(err)
+		// }
 
 		errWrite := wArch.Close()
-		if errWrite != nil {
-			log.Fatal(errWrite)
-		}
+		check(errWrite)
 
 		a, _ := os.Open("./archive.zip")
 		reader := bufio.NewReader(a)
@@ -77,9 +84,7 @@ func main() {
 		w.Write([]byte(encoded))
 
 		e := os.Remove("./archive.zip")
-		if e != nil {
-			log.Fatal(e)
-		}
+		check(e)
 	})
 	log.Fatal(http.ListenAndServe(":5050", nil))
 }
