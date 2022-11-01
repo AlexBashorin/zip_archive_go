009490af696d4005cacaf0c287fafabe717ec359
Earn <alexander.bashorin@yandex.ru>
2022-11-01 23:06:42 +0400

    теперь получаю с помощью FormData

--- zipit/main.go
+++ zipit/main.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func check(e error) {
@@ -25,19 +24,74 @@ func main() {
 	// write zip
 	http.HandleFunc("/zipit", func(w http.ResponseWriter, r *http.Request) {
 		// PARSE FORM
-		// var buf bytes.Buffer
-		errPF := r.ParseMultipartForm(32 << 20)
-		// errPF := r.ParseForm()
-		check(errPF)
+		// // var buf bytes.Buffer
+		// errPF := r.ParseMultipartForm(32 << 20)
+		// check(errPF)
 
-		file, header, errFile := r.FormFile("fileOne")
-		check(errFile)
+		// file, handler, errFile := r.FormFile("fileOne")
+		// check(errFile)
+		// defer file.Close()
+
+		// // f, errOS := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		// fFORM, errOS := os.Create(handler.Filename)
+		// check(errOS)
+		// defer fFORM.Close()
+
+		// // bodyFILE, err := iotuil.ReadFile("file.txt")
+		// // if err != nil {
+		// // 	log.Fatalf("unable to read file: %v", err)
+		// // }
+
+		// n := "./" + handler.Filename
+		// openFILE, _ := os.Open(n)
+		// readerFILE := bufio.NewReader(openFILE)
+		// contentFILE, _ := ioutil.ReadAll(readerFILE)
+
+		// // nameFile := strings.Split(header.Filename, ".")
+		// // ff, errFF := ioutil.ReadFile(header.Filename)
+		// // check(errFF)
+
+		file, fileHeader, err := r.FormFile("fileOne")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
 		defer file.Close()
 
-		nameFile := strings.Split(header.Filename, ".")
-		ff, errFF := ioutil.ReadFile(header.Filename)
-		check(errFF)
+		buff := make([]byte, fileHeader.Size)
+		_, err = file.Read(buff)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
+		// // Create the uploads folder if it doesn't
+		// // already exist
+		// err = os.MkdirAll("./uploads", os.ModePerm)
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	return
+		// }
+
+		// // Create a new file in the uploads directory
+		// dst, err := os.Create(fmt.Sprintf("./uploads/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	return
+		// }
+
+		// defer dst.Close()
+
+		// // Copy the uploaded file to the filesystem
+		// // at the specified destination
+		// _, err = io.Copy(dst, file)
+		// if err != nil {
+		// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+		// 	return
+		// }
+
+		//////// BASE64
 		// b, err := io.ReadAll(r.Body)
 		// check(err)
 
@@ -60,10 +114,11 @@ func main() {
 
 		wArch := zip.NewWriter(archive)
 
-		f, err := wArch.Create(nameFile[0])
+		fileZip, err := wArch.Create(fileHeader.Filename)
 		check(err)
-		_, err = f.Write(ff)
+		_, err = fileZip.Write(buff)
 		check(err)
+
 		// for _, file := range files {
 		// 	f, err := wArch.Create(file.Name)
 		// 	check(err)
